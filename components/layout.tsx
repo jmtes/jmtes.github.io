7dca4e8a55944323c4ef864db7a0880beae66119
jmtes <juliennetesoro@gmail.com>
2020-12-08 11:28:47 -0800

    Style layout typography

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,24 +1,7 @@
-// COMPONENT-LEVEL STYLES WITH CSS MODULES
-// This is a layout component that can be shared across all pages and
-// contains styling.
-
 import Head from 'next/head';
 import Link from 'next/link';
 
-// Importing a CSS module
 import styles from './layout.module.css';
-
-// To use the imported CSS module's container class, simply use
-// styles.container as the returned div's className.
-
-// If you inspect the HTML, you'll notice that a unique class name was
-// automatically generated for the div. So long as you use CSS Modules or
-// some other CSS-in-JS library, you needn't worry about class name
-// collisions.
-
-// What's more is that Next.js's code splitting applies to CSS Modules as
-// well. It ensures the minimal amount of CSS is loaded for each page,
-// resulting in smaller bundle sizes.
 
 const name = 'Juno Tesoro';
 export const siteTitle = 'Juno Tesoro';
@@ -30,10 +13,15 @@ const Layout = ({
   children: React.ReactNode;
   home?: boolean;
 }) => (
-  <div className={styles.container}>
+  <div className={`${styles.container}`}>
     <Head>
       <link rel='icon' href='/favicon.ico' />
       <meta name='description' content='This is my portfolio site!' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap'
+        rel='stylesheet'
+      />
     </Head>
     {!home && (
       <div className='sidebar'>
