53d036c886632c2b18a88b24e32a62efcc84afc2
jmtes <juliennetesoro@gmail.com>
2020-12-08 10:29:15 -0800

    Remove extraneous <main> tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,29 +8,28 @@ const Home = () => (
     <Head>
       <title>{siteTitle}</title>
     </Head>
-    <main>
-      <img src='images/profile.jpg' alt='A picture of me' />
-      <h1>Hi, I'm Juno! I'm a software developer.</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/about'>
-              <a>Read about me</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/portfolio'>
-              <a>See my portfolio</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/contact'>
-              <a>Contact me</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </main>
+
+    <img src='images/profile.jpg' alt='A picture of me' />
+    <h1>Hi, I'm Juno! I'm a software developer.</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link href='/about'>
+            <a>Read about me</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/portfolio'>
+            <a>See my portfolio</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/contact'>
+            <a>Contact me</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </Layout>
 );
 
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -8,30 +8,29 @@ const Portfolio = () => (
     <Head>
       <title>{`${siteTitle} - Portfolio`}</title>
     </Head>
-    <main>
-      <h2>Portfolio</h2>
-      <Project
-        name='Bearbnb'
-        technologies='GraphQL, Prisma, and PostgreSQL'
-        desc="A simple clone of the Airbnb API. Allows users to create accounts, upload listings, browse and book others' listings, and write reviews for them, among other things."
-        links={[
-          {
-            href: 'https://bearbnb-api.herokuapp.com/',
-            text: 'GraphQL Playground'
-          },
-          { href: 'https://github.com/jmtes/bearbnb-server', text: 'Github' }
-        ]}
-      />
-      <Project
-        name='mybadtweets'
-        technologies='Node and Vanilla JS'
-        desc='You log in with Twitter and it shows you your least popular tweets as a slideshow, warranting that you have to see each and every one of them. You can then delete individual tweets out of shame or retweet them to give them another chance right from the app without needing to go to Twitter and search for them yourself.'
-        links={[
-          { href: 'https://mybadtweets.herokuapp.com', text: 'Demo' },
-          { href: 'https://github.com/jmtes/mybadtweets', text: 'Github' }
-        ]}
-      />
-    </main>
+
+    <h2>Portfolio</h2>
+    <Project
+      name='Bearbnb'
+      technologies='GraphQL, Prisma, and PostgreSQL'
+      desc="A simple clone of the Airbnb API. Allows users to create accounts, upload listings, browse and book others' listings, and write reviews for them, among other things."
+      links={[
+        {
+          href: 'https://bearbnb-api.herokuapp.com/',
+          text: 'GraphQL Playground'
+        },
+        { href: 'https://github.com/jmtes/bearbnb-server', text: 'Github' }
+      ]}
+    />
+    <Project
+      name='mybadtweets'
+      technologies='Node and Vanilla JS'
+      desc='You log in with Twitter and it shows you your least popular tweets as a slideshow, warranting that you have to see each and every one of them. You can then delete individual tweets out of shame or retweet them to give them another chance right from the app without needing to go to Twitter and search for them yourself.'
+      links={[
+        { href: 'https://mybadtweets.herokuapp.com', text: 'Demo' },
+        { href: 'https://github.com/jmtes/mybadtweets', text: 'Github' }
+      ]}
+    />
   </Layout>
 );
 
