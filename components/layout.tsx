b0f0d45ec0c9cc42b4131d9c192305fda149c0a7
jmtes <juliennetesoro@gmail.com>
2020-12-08 17:10:22 -0800

    Style sidebar

--- components/layout.tsx
+++ components/layout.tsx
@@ -27,36 +27,38 @@ const Layout = ({
       />
     </Head>
     {!home && (
-      <div className='sidebar'>
-        <img
-          src='images/profile.jpg'
-          alt='A picture of me'
-          className='sidebar__img'
-        />
-        <h1 className='sidebar__title'>
-          <Link href='/'>
-            <a>Juno Tesoro</a>
-          </Link>
-        </h1>
-        <nav className='sidebar__nav'>
-          <ul>
-            <li>
-              <Link href='/about'>
-                <a>About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/portfolio'>
-                <a>Portfolio</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/contact'>
-                <a>Contact</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <div className={styles.sidebar}>
+        <div className={styles['sidebar__content']}>
+          <img
+            src='images/profile.jpg'
+            alt='A picture of me'
+            className={styles['sidebar__img']}
+          />
+          <h1 className={styles['sidebar__title']}>
+            <Link href='/'>
+              <a>Juno Tesoro</a>
+            </Link>
+          </h1>
+          <nav className={styles['sidebar__nav']}>
+            <ul>
+              <li>
+                <Link href='/about'>
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/portfolio'>
+                  <a>Portfolio</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/contact'>
+                  <a>Contact</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     )}
     <main className={home && 'index'}>{children}</main>
