b8deaf873aae2ddd7d828b314254058ff0c22ec5
jmtes <juliennetesoro@gmail.com>
2020-12-11 23:13:16 -0800

    Specify charset

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,6 +22,7 @@ const Layout = ({
     <Head>
       <link rel='icon' href='/favicon.ico' />
       <meta name='description' content='This is my portfolio site!' />
+      <meta charSet='UTF-8' />
       <link rel='preconnect' href='https://fonts.gstatic.com' />
       <link
         href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap'
